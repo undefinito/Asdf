848c7fc61f9f413becb82f8ff2ab2e8d2752e943
hindi ako si ned <hindakosned@gmail.com>
2013-07-08 23:00:34 +0800

    OCD
    OGC

--- db_seed-data.sql
+++ db_seed-data.sql
@@ -15,5 +15,14 @@ INSERT INTO Teacher (teacher_ID, first_name, last_name, middle_initial, teacher_
 	( '200307418201', 'Christine', 'Gendrano', 'A', '11' ),
 	( '200307412203', 'Ver', 'Ramos', 'C', '14' ),
 	( '200507413404', 'Jordan', 'Deja', 'B', '13' ),
-	( '200707412412', 'Consuelo', 'Santos', 'B', '12' ),
+	( '200707412412', 'Consuelo', 'Santos', 'B', '12' )
+	;
+	
+--	STUDENT	----------------
+
+INSERT INTO Student (student_id, first_name, middle_initial, last_name)
+	VALUES
+	('201207411967', 'Jose', 'K', 'Abalos II'),
+	('201207412399', 'Ria Angela', 'P', 'Alejar'),
+	('201207412038', 'Ramon Paolo', 'H', 'Alfar')
 	;
