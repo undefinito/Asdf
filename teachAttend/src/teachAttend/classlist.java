680852370165b4e65b139c3f9cb94319038f59ae
Carlo Carabeo <carlo.carabeo@gmail.com>
2013-08-24 00:04:52 +0800

    Teacher Attendance Project
    Di pa me tapos <3

---
+++ teachAttend/src/teachAttend/classlist.java
@@ -0,0 +1,114 @@
+package teachAttend;
+
+//Author : Carlo Carabeo
+public class classlist
+{
+	private String teacher_ID;
+	private String coursecode;
+	private String first_name;
+	private String middle_name;
+	private String last_name;
+	private String sched_room;
+	private String sched_time;
+	private String sched_day;
+
+	/* 
+SELECT teacher.teacher_ID, course_name, first_name, last_name, middle_initial, sched_room, sched_time, sched_day
+FROM classlist, teacher, course
+WHERE classlist.teacher_ID = teacher.teacher_ID
+AND classlist.course_ID = course.course_ID
+	 */
+	
+	public classlist( String ID, String f_name, String l_name, String mi, String course_code, String room, String time, String day )
+	{
+        teacher_ID = ID;
+		first_name = f_name;
+		middle_name = mi;
+		last_name = l_name;
+		coursecode = course_code;
+		sched_room = room;
+		sched_time = time;
+		sched_day = day;
+	}
+
+	//@return the teacher_ID
+	public String getTeacher_ID() {
+		return teacher_ID;
+	}
+
+	//@return the classcode
+	public String getCoursecode() {
+		return coursecode;
+	}
+
+	//@return the first_name
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	//@return the middle_name
+	public String getMiddle_name() {
+		return middle_name;
+	}
+
+	//@return the last_name
+	public String getLast_name() {
+		return last_name;
+	}
+
+	//@return the sched_room
+	public String getSched_room() {
+		return sched_room;
+	}
+
+	//@return the sched_time
+	public String getSched_time() {
+		return sched_time;
+	}
+
+	//@return the sched_day
+	public String getSched_day() {
+		return sched_day;
+	}
+
+	//@param teacher_ID the teacher_ID to set
+	public void setTeacher_ID(String teacher_ID) {
+		this.teacher_ID = teacher_ID;
+	}
+
+	//@param classcode the coursecode to set
+	public void setClasscode(String classcode) {
+		this.coursecode = classcode;
+	}
+
+	//@param first_name the first_name to set
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	//@param middle_name the middle_name to set
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+
+	//@param last_name the last_name to set
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	//@param sched_room the sched_room to set
+	public void setSched_room(String sched_room) {
+		this.sched_room = sched_room;
+	}
+
+	//@param sched_time the sched_time to set
+	public void setSched_time(String sched_time) {
+		this.sched_time = sched_time;
+	}
+
+	//@param sched_day the sched_day to set
+	public void setSched_day(String sched_day) {
+		this.sched_day = sched_day;
+	}
+
+}
---
+++ teachAttend/src/teachAttend/java_sql.java
@@ -0,0 +1,136 @@
+package teachAttend;
+
+import java.sql.*;
+import java.util.*;
+
+public class java_sql
+{
+	
+	public Connection con;	//variable for the connection
+	public Statement statement;//object for making sql statements
+	public ResultSet resultSet;//object that receives sql results
+	
+	private final String user = "root";
+	private final String pass = "password";
+	private final String location = "localhost";
+	private final String DBName = "syanad2";
+	private final String port = ":3306"; // add : then port number
+	private final String driverLoc = "com.mysql.jdbc.Driver";
+	private final String URL = "jdbc:mysql://" + location + port + "/" + DBName;
+
+	public java_sql( )
+	{
+		con = null;
+		statement = null;
+		resultSet = null;
+	}
+	
+	private void connect(){//maybe private
+		//COMMENTED OUT
+		//for debugging db connections
+		try {
+			Class.forName(driverLoc);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Mysql JDBC driver is Missing ");
+			e.printStackTrace();
+			return; 			//apparently this stops running the remaining code
+		}
+		//System.out.println("JDBC driver loaded");
+		
+		//Checks The existence of the Database and authenticates the user 
+		try{
+			con = DriverManager.getConnection(URL, user, pass);
+		} catch(SQLException e){
+			System.out.println("Database does not exist or authentication failed ");
+			e.printStackTrace();
+			return;
+		}
+		//System.out.println("Authentication Complete");
+		//System.out.println("Database found");
+		
+		if(con == null){
+			System.out.println("Connection Failed");
+		}/*
+		else{
+			System.out.println("Connected to " + DBName + " on " + location);
+		}*/
+		//END CONNECTION
+	}
+        
+        private int getNumRows()
+        {
+            int size = 0;
+            try {
+                resultSet.last();
+                size = resultSet.getRow();
+                resultSet.beforeFirst();
+            }
+            catch(Exception ex) {
+                return 0;
+            }
+            return size;
+        }
+	
+       
+	public String[][]query(String q)
+        {
+            //ArrayList of arrays(of Strings) yo dawg
+            String[][] st ={{""}};
+                
+                int i=0;
+                int numOfColumns;   //number of columns of resultSet
+                int numOfRows;      //number of rows of resultSet
+                
+                connect();
+		
+                if(con != null)
+                {
+		//Testing of Queries
+                    try { 
+				statement = con.createStatement();// creates an object used for sending sql statements to the database
+				resultSet = statement.executeQuery(q);// receives the result set of the querty
+				
+                                //<editor-fold defaultstate="collapsed" desc="get number of columns">
+                                
+                                //get metadata to count resulting columns
+                                ResultSetMetaData rsmd = resultSet.getMetaData();
+                                
+                                //get the number of columns
+                                numOfColumns = rsmd.getColumnCount();
+                                
+                                numOfRows = getNumRows();
+                                
+                                //</editor-fold>
+                                
+//                               for debugging
+//                               System.out.println(numOfColumns);
+//                               System.out.println(numOfRows);
+                               
+                                String[][] temps = new String[numOfRows][numOfColumns];
+                                while(resultSet.next())
+                                {
+                                    //iterate through columns
+                                    for(int j = 0; j<numOfColumns; j++)
+                                    {
+                                       temps[i][j] = resultSet.getString(j+1);
+                                    }
+                                    
+                                    i++;//next row in resultSet matrix
+                                }
+                                
+				//Close connections
+				resultSet.close();
+				statement.close();
+				con.close();
+                                
+                               st = temps;
+                                
+			} catch (Exception e){
+				e.printStackTrace();
+			} 
+		}
+                
+                return st;
+	}
+	
+}
